b5bb232fcdfe4daca8e56f14500dbb5ba6457839
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2020-05-06 21:46:21 +0200

    migrate to angular 9.x
    - source: https://nx.dev/angular/workspace/update
    - cmd: nx migrate @nrwl/workspace

--- libs/angular-fixtures/src/public_api.ts
+++ libs/angular-fixtures/src/public_api.ts
@@ -5,5 +5,6 @@
  */
 export * from './lib/fixtures/setup.fixture';
 export * from './lib/fixtures/test-bed-setup.fixture';
+export * from './lib/stubs/router-link-directive.stub';
 export * from './lib/utils/fixture.util';
 export * from './lib/views/simple-component.view';
